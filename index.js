20b3825c56103740675b305e1cfd488cf1caddb9
Urey Miller <rezept01.zeckern@icloud.com>
2023-01-03 21:30:03 +0100

    finished styling and ringtone

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function App() {
   const initState = {
     time: {
-      session: 5,
-      break: 10
+      session: 25 * 60,
+      break: 5 * 60
     },
-    mode: "default",
+    mode: 'default',
     isTicking: false,
-    isResuming: false
+    isResuming: false,
+    pushedButton: '',
   }
 
   const [time, setTime] = React.useState(initState.time);
@@ -14,9 +15,12 @@ function App() {
   const [mode, setMode] = React.useState(initState.mode);
   const [isTicking, toggleTicking] = React.useState(initState.isTicking);
   const [isResuming, toggleResuming] = React.useState(initState.isResuming);
+  const [pushedPlus, togglePlus] = React.useState(initState.pushedButton);
+  const [pushedMode, toggleMode] = React.useState(initState.pushedButton);
+  const [pushedMinus, toggleMinus] = React.useState(initState.pushedButton);
 
   function startSession() {
-    setMode("session");
+    setMode('session');
     let intervalId = setInterval(() => {
       setCurrTime(prev => {
         return {
@@ -26,20 +30,21 @@ function App() {
       });
     }, 1000);
     
-    localStorage.setItem("interval-id", intervalId);
+    localStorage.setItem('interval-id', intervalId);
     
     let timeoutId = setTimeout(() => {
-      alert("RINGG!!");
+      const audio = new Audio('./media/ring.m4a');
+      audio.play();
       clearInterval(intervalId);
       setCurrTime(time);
       startBreak();
-    }, currTime.session * 1000 + 500);
+    }, currTime.session * 1000);
     
-    localStorage.setItem("timeout-id", timeoutId);
+    localStorage.setItem('timeout-id', timeoutId);
   }
 
   function startBreak() {
-    setMode("break");
+    setMode('break');
     let intervalId = setInterval(() => {
       setCurrTime(prev => {
         return {
@@ -49,16 +54,17 @@ function App() {
       });
     }, 1000);
     
-    localStorage.setItem("interval-id", intervalId);
+    localStorage.setItem('interval-id', intervalId);
     
     let timeoutId = setTimeout(() => {
-      alert("RINGG!!");
+      const audio = new Audio('./media/ring.m4a');
+      audio.play();
       clearInterval(intervalId);
       setCurrTime(time);
       startSession();
-    }, currTime.break * 1000 + 500);
+    }, currTime.break * 1000);
     
-    localStorage.setItem("timeout-id", timeoutId);
+    localStorage.setItem('timeout-id', timeoutId);
   }
 
   function stopTicking() {
@@ -69,93 +75,107 @@ function App() {
 
   return (
     <main>
-      <div className="top-buttons">
-        <div className="button-wrapper plus-button">
+      <div className='top-buttons'>
+        <div className='button-wrapper plus-button'>
           <button onClick={() => {
+            togglePlus('button-pushed');
+            setTimeout(() => togglePlus(''), 500);
             if(isTicking) return;
-            setCurrTime(changeTime(mode, currTime, "+"));
-            setTime(changeTime(mode, time, "+"));
-          }}>
+            setCurrTime(changeTime(mode, currTime, '+'));
+            setTime(changeTime(mode, time, '+'));
+          }} className={pushedPlus}>
           </button>
-          <div className="button-neck"></div>
+          <div className='button-neck'></div>
           <p>Plus</p>
         </div>
         
-        <div className="button-wrapper mode-button">
+        <div className='button-wrapper mode-button'>
           <button onClick={() => {
+            toggleMode('button-pushed');
+            setTimeout(() => toggleMode(''), 500);
             if(isTicking) return;
             setMode(changeMode(mode));
-          }}>
+          }} className={pushedMode}>
           </button>
-          <div className="button-neck"></div>
+          <div className='button-neck'></div>
           <p>Mode</p>
         </div>
-
-        <div className="button-wrapper minus-button">
+        
+        <div className='button-wrapper minus-button'>
           <button onClick={() => {
+            toggleMinus('button-pushed');
+            setTimeout(() => toggleMinus(''), 500);
             if(isTicking) return;
-            setCurrTime(changeTime(mode, currTime, "-"));
-            setTime(changeTime(mode, time, "-"));
-          }}>
+            setCurrTime(changeTime(mode, currTime, '-'));
+            setTime(changeTime(mode, time, '-'));
+          }} className={pushedMinus}>
           </button>
-          <div className="button-neck"></div>
+          <div className='button-neck'></div>
           <p>Minus</p>
         </div>
       </div>
       
-      <img className="title-svg" src="./media/h1.svg" alt="title" />
-      
-      <hr />
-      
-      <div className="display-wrapper">
-        <TimeDisplay title="session" time={currTime.session} mode={mode}/>
-        <TimeDisplay title="break" time={currTime.break} mode={mode}/>
-      </div>
-      <div className="controll-wrapper">
-        <button onClick={() => {
-          if(isTicking) return;
-          toggleTicking(true);
-          if(!isResuming) {
-            setMode('session');
-            startSession();
-          } else {
-            switch(mode) {
-              case "session":
-                startSession();
-                break;
-              case "break":
-                startBreak();
-                break;
-              default:
-                startSession();
-                break;
+      <div className='inner-main'>
+        <img className='title-svg' src='./media/h1.svg' alt='title' />
+       
+        <hr />
+        
+        <div className='display-wrapper'>
+          <TimeDisplay title='session' time={currTime.session} mode={mode}/>
+          <TimeDisplay title='break' time={currTime.break} mode={mode}/>
+        </div>
+        
+        <div className='controll-wrapper'>
+          <button onClick={() => {
+            console.log(isTicking);
+            if(isTicking) return;
+            toggleTicking(true);
+            if(!isResuming) {
+              setMode('session');
+              startSession();
+            } else {
+              switch(mode) {
+                case 'session':
+                  startSession();
+                  break;
+                case 'break':
+                  startBreak();
+                  break;
+                default:
+                  startSession();
+                  break;
+              }
             }
-          }
-        }}>Start</button>
-        <button onClick={() => {
-          toggleTicking(false);
-          setCurrTime(time);
-          setMode("default");
-          stopTicking();
-          toggleResuming(false);
-        }}>Reset</button>
-        <button onClick={() => {
-          stopTicking();
-          toggleResuming(true);
-        }}>Stop</button>
-      </div>
+          }}>Start</button>
+          
+          <button onClick={() => {
+            toggleTicking(false);
+            setCurrTime(time);
+            setMode('default');
+            stopTicking();
+            toggleResuming(false);
+          }}>Reset</button>
+          <button onClick={() => {
+            stopTicking();
+            toggleResuming(true);
+            toggleTicking(false);
+          }}>Stop</button>
+        </div>
+        
+        <p className='creator'>By Urey Miller</p>
+      </div>        
     </main>
   );
 }
 
 function TimeDisplay(props) {
-  const classMode = props.title == props.mode ? " highlighted" : ""; 
-  const className = "displays" + classMode;
+  const classMode = props.title == props.mode ? ' highlighted' : ''; 
+  const className = 'displays' + classMode;
 
   return (
     <div className={className}>
-      <h2 className="display-title">{props.title}</h2>
-      <p className="display-time">{convertTime(props.time)}</p>
+      <h2 className='display-title'>{props.title}</h2>
+      <p className='display-time'>{convertTime(props.time)}</p>
     </div>
   );
 }
@@ -163,26 +183,26 @@ function TimeDisplay(props) {
 function convertTime(secs) {
   let minutes = Math.floor(secs / 60);
   let seconds = secs % 60;
-  if(minutes < 10) minutes = "0" + minutes;
-  if(seconds < 10) seconds = "0" + seconds;
-  return minutes + ":" + seconds;
+  if(minutes < 10) minutes = '0' + minutes;
+  if(seconds < 10) seconds = '0' + seconds;
+  return minutes + ':' + seconds;
 }
 
 function changeTime(currMode, currTime, operation) {
   if(checkIfCapped(currMode, currTime, operation)) return currTime;
 
   switch(currMode) {
-    case "session":
+    case 'session':
       return {
-        session: operation == "+" ?
+        session: operation == '+' ?
           toFullMinutes(currTime.session + 60) :
           toFullMinutes(currTime.session - 60),
         break: currTime.break
       };
-    case "break":
+    case 'break':
       return {
         session: currTime.session,
-        break: operation == "+" ?
+        break: operation == '+' ?
           toFullMinutes(currTime.break + 60) :
           toFullMinutes(currTime.break - 60)
       };
@@ -197,26 +217,26 @@ function toFullMinutes(secs) {
 
 function checkIfCapped(currMode, currTime, operation) {
   switch(operation) {
-    case "+":
+    case '+':
       return currTime[currMode] >= 99 * 60;
-    case "-":
+    case '-':
       return currTime[currMode] <= 1 * 60;
   }
 }
 
 function changeMode(currMode) {
   switch(currMode) {
-    case "default":
-      return "session";
-    case "session":
-      return "break";
-    case "break":
-      return "default";
+    case 'default':
+      return 'session';
+    case 'session':
+      return 'break';
+    case 'break':
+      return 'default';
     default:
       return currMode;
   }
 }
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
